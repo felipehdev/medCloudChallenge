4be10d44b91a8d1e98af614194f03e0e282a5b88
felipr <100290286+felipehdev@users.noreply.github.com>
2022-11-22 21:22:40 -0300

    last commit

--- cliniCloud/src/components/PatientTable/PatientTable.tsx
+++ cliniCloud/src/components/PatientTable/PatientTable.tsx
@@ -77,8 +77,8 @@ const PatientTable = () => {
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
         disableSelectionOnClick
 
         onRowClick={(params)=> {setRowId(params);}}
